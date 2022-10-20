2c5d424310b91faa48309a28a31a83f0035326da
zhqing <9740878@qq.com>
2022-10-20 17:50:39 +0800

    增加acme检查ssl更新ssl方法

--- src/extend/frame/Acme.php
+++ src/extend/frame/Acme.php
@@ -29,4 +29,38 @@ trait Acme {
         \exec("{$acme} --renew -d {$domain} --config-home {$sslFile} --force", $K, $v);
         return $v == 0;
     }
+
+    /**
+     * @param $cerPem //检查的cer或者pem文件
+     * @param string $domain //all=更新全部,[域名单个逗号分开]
+     * @param string $acme //acme 路径
+     * @return mixed
+     */
+    public static function updateSsl($cerPem, string $domain = 'all', string $acme = '/usr/local/acme.sh/acme.sh'): mixed {
+        \date_default_timezone_set('Asia/Shanghai');
+        if (empty(is_file($cerPem))) {
+            return $cerPem . '[does not exist]';
+        }
+        $cert = \openssl_x509_parse(file_get_contents($cerPem));
+        $time = $cert['validTo_time_t'] ?? 0;
+        if (empty($time)) {
+            return 'validTo_time_t error';
+        }
+        //7天内更新SSL
+        if (($time - time()) <= 604800) {
+            if ($domain == 'all') {
+                \exec($acme . " --renew-all --force", $arr, $is);
+            } else {
+                $dArr = explode(',', $domain);
+                $d = '';
+                foreach ($dArr as $v) {
+                    $d .= '-d ' . $v;
+                }
+                \exec($acme . " --renew " . $d . " --force", $arr, $is);
+            }
+            return ($is == 0) ? join("\r\n", $arr) : 'acme error';
+        } else {
+            return ($time - time());
+        }
+    }
 }
