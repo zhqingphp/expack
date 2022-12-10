9f709a566682f42cfea5045869e4a369dad0220f
zhqing <9740878@qq.com>
2022-12-11 06:17:25 +0800

    增加方法

---
+++ src/workerman/Back.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace zhqing\workerman;
+
+use zhqing\extend\Frame;
+
+class Back {
+    private array $Data = [
+        'int' => true,
+        'code' => '',
+        'data' => '',
+        'msg' => '',
+        'success' => '',
+        'status' => '',
+        'count' => ''
+    ];
+
+    /**
+     * @param int $Data
+     * @return $this
+     */
+    public static function code(int $Data): static {
+        $Self = new self();
+        $Self->Data['code'] = $Data;
+        return $Self;
+    }
+
+    /**
+     * @param $Data
+     * @return $this
+     */
+    public function data($Data): static {
+        $this->Data['data'] = $Data;
+        return $this;
+    }
+
+    /**
+     * @param $Data
+     * @return $this
+     */
+    public function msg($Data): static {
+        $this->Data['msg'] = $Data;
+        return $this;
+    }
+
+    /**
+     * @param $Data
+     * @return $this
+     */
+    public function success($Data): static {
+        $this->Data['success'] = $Data;
+        return $this;
+    }
+
+    /**
+     * 数量
+     * @param $Data
+     * @return $this
+     */
+    public function count($Data): static {
+        $this->Data['count'] = $Data;
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function array(): array {
+        if (is_array($this->Data['data']) && empty($this->Data['count'])) {
+            $this->Data['count'] = count($this->Data['data']);
+        }
+        return $this->Data;
+    }
+
+    /**
+     * @param bool $Type
+     * @return response
+     */
+    public function json(bool $Type = false): response {
+        return \response(Frame::json($this->array(), $Type), 200, ['Content-Type' => 'application/javascript']);
+    }
+}
--- src/workerman/Way.php
+++ src/workerman/Way.php
@@ -2,20 +2,25 @@
 
 namespace zhqing\workerman;
 class Way {
-    public static function cacheJs() {
+    /**
+     * @param array $list //追加js
+     * @param bool $type /是否更新
+     * @return mixed
+     */
+    public static function cacheJs(array $list = [], bool $type = false) {
         $dir = __DIR__ . '/../../file';
         $file = $dir . '/cache.js';
-        if (is_file($file)) {
+        if (is_file($file) && empty($type)) {
             return \response()->file($file);
         }
-        $arr = [
+        $arr = array_merge([
             $dir . '/js/jquery.min.js',
             $dir . '/js/clipboard.js',
             $dir . '/js/jquery.qrcode.min.js',
             $dir . '/js/CryptoJS.js',
             $dir . '/js/jsencrypt.js',
             $dir . '/js/axios.min.js',
-        ];
+        ], $list);
         $body = '';
         foreach ($arr as $v) {
             if (is_file($v)) {
