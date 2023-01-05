f0245fca80ba422890b90b7ba1de2a4f51c1f2d4
zhqing <9740878@qq.com>
2023-01-06 01:20:03 +0800

    frame

--- src/extend/Frame.php
+++ src/extend/Frame.php
@@ -145,10 +145,10 @@ class Frame {
     /**
      * 获取文件名
      * @param string $str 字符串
-     * @param string|null $format 是否删除格式(结尾要删除的内容)
+     * @param string $format 是否删除格式(结尾要删除的内容)
      * @return string
      */
-    public static function baseName(string $str, string $format = null): string {
+    public static function baseName(string $str, string $format = ''): string {
         return \basename($str, $format);
     }
 
@@ -510,5 +510,49 @@ class Frame {
         } else {
             return $m . 'm';
         }
+    }
+
+    /**
+     * 获取语义化时间
+     * @param $time
+     * @return string
+     */
+    public static function humanDate($time): string {
+        $timestamp = is_numeric($time) ? $time : strtotime($time);
+        $dur = time() - $timestamp;
+        if ($dur < 0) {
+            return date('Y-m-d', $timestamp);
+        } else {
+            if ($dur < 60) {
+                return $dur . '秒前';
+            } else {
+                if ($dur < 3600) {
+                    return floor($dur / 60) . '分钟前';
+                } else {
+                    if ($dur < 86400) {
+                        return floor($dur / 3600) . '小时前';
+                    } else {
+                        if ($dur < 2592000) { // 30天内
+                            return floor($dur / 86400) . '天前';
+                        }
+                    }
+                }
+            }
+        }
+        return date('Y-m-d', $timestamp);
+    }
+
+    /**
+     * 格式化文件大小
+     * @param $file_size
+     * @return string
+     */
+    public static function formatBytes($file_size): string {
+        $size = sprintf("%u", $file_size);
+        if ($size == 0) {
+            return ("0 Bytes");
+        }
+        $size_name = array(" Bytes", " KB", " MB", " GB", " TB", " PB", " EB", " ZB", " YB");
+        return round($size / pow(1024, ($i = floor(log($size, 1024)))), 2) . $size_name[$i];
     }
 }
