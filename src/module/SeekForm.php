543c4e4b1d6d37d36adad91232bc5db5a2eb7bfc
zhqing <9740878@qq.com>
2023-03-14 16:57:12 +0800

    修复问题

--- src/module/SeekForm.php
+++ src/module/SeekForm.php
@@ -1,28 +1,21 @@
 <?php
 
-namespace package\tron;
+namespace zhqing\module;
 
 use zhqing\extend\Frame;
 
-class Seek {
+class SeekForm {
+    private array $body = [];
 
-    public array|string $SEEK_DATA = [];
-    public string $SEEK_TYPE = '';
-
-    public function __construct(array|string $data, string $type = '') {
-        $this->SEEK_DATA = $data;
-        $this->SEEK_TYPE = $type;
+    public function __construct(array|string $data) {
+        $this->body = is_array($data) ? $data : Frame::isJson($this->body);
     }
 
     public function __call($method, $parameter) {
-        return Frame::getStrArr((is_array($this->SEEK_DATA) ? $this->SEEK_DATA : Frame::isJson($this->SEEK_DATA)), (!empty($this->SEEK_TYPE) ? $this->SEEK_TYPE . '.' : '') . $method . (isset($parameter[0]) ? '.' . $parameter[0] : ''), ($parameter[1] ?? ''));
+        return Frame::getStrArr($this->body, $method . (isset($parameter[0]) ? '.' . $parameter[0] : ''), ($parameter[1] ?? ''));
     }
 
-    public function getBody(): array|string {
-        return $this->SEEK_DATA;
-    }
-
-    public function toJson(): string {
-        return (is_array($this->SEEK_DATA) ? Frame::json($this->SEEK_DATA) : $this->SEEK_DATA);
+    public function getJsonData(): bool|string {
+        return Frame::json($this->body);
     }
 }
