920579d7edebb4b0964e95f347c54cf25d52739b
zhqing <9740878@qq.com>
2023-06-04 14:42:37 +0800

    .

--- src/module/IpIdea.php
+++ src/module/IpIdea.php
@@ -4,19 +4,64 @@ namespace zhqing\module;
 
 use zhqing\extend\Frame;
 
-class ipIdea {
-    public static array $arr = [];
-
+/**
+ * https://www.ipidea.net/
+ */
+class IpIdea {
     /**
+     * 动态数据中心
+     * @param string $user //帐号
+     * @param string $pass //密码
+     * @param string $region //选择地区
+     * @param string $ip //ip
+     * @param string $port //端口
      * @return array
      */
-    public static function getArr(): array {
-        $data = Frame::getStrArr(Frame::isJson(@file_get_contents(__DIR__ . '/../../file/ipIdea.json')), 'ret_data', []);
-        self::$arr = [];
-        foreach ($data as $v) {
-            self::$arr[$v['country_code']] = $v['country_name'];
-        }
-        return self::$arr;
+    public static function static(string $user, string $pass, string $region = '', string $ip = '', string $port = ''): array {
+        $data['ip'] = 'na.ipidea.io';
+        $data['port'] = '2336';
+        $data['userPass'] = $user . '-zone-static' . (!empty($region) ? ('-region-' . $region) : '') . ':' . $pass;
+        $data['type'] = 'http';
+        $data['auth'] = 'basic';
+        return $data;
     }
 
+    /**
+     * 全球动态
+     * @param string $user //帐号
+     * @param string $pass //密码
+     * @param string $region //选择地区
+     * @param string $ip //ip
+     * @param string $port //端口
+     * @return array
+     */
+    public static function custom(string $user, string $pass, string $region = '', string $ip = '', string $port = ''): array {
+        $data['ip'] = $ip;
+        $data['port'] = $port;
+        $data['userPass'] = $user . '-zone-custom' . (!empty($region) ? ('-region-' . $region) : '') . ':' . $pass;
+        $data['type'] = 'http';
+        $data['auth'] = 'basic';
+        return $data;
+    }
+
+    /**
+     * 获取地区列表
+     * @param array $arr
+     * @return array
+     */
+    public static function getArr(array $arr = []): array {
+        $arr = [];
+        $dir = __DIR__ . '/../../file/';
+        $file = rtrim($dir, '/') . '/IpIdea_code.json';
+        if (empty(is_file($file))) {
+            $data = Frame::getStrArr(Frame::isJson(@file_get_contents(rtrim($dir, '/') . '/IpIdea.json')), 'ret_data', []);
+            foreach ($data as $v) {
+                $arr = [$v['country_code']] = $v['country_name'];
+            }
+            file_put_contents($file, Frame::json($arr));
+        } else {
+            $arr = Frame::isJson(@file_get_contents($file));
+        }
+        return $arr;
+    }
 }
