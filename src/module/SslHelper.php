2024815e4f5ae09352a00c0eac15401578e83fb2
zhqing <9740878@qq.com>
2024-02-19 02:58:50 +0800

    ssl update

--- src/module/SslHelper.php
+++ src/module/SslHelper.php
@@ -3,34 +3,152 @@
 namespace zhqing\module;
 
 class SslHelper {
-
-    public array $data = [];
-
     /**
+     * acme转换各种环境
      * @param string $path acme 证书位置
      * @param string $save 保存路径
      * @return string|array,success=成功,array=失败信息
      */
     public static function acme(string $path, string $save): string|array {
-        return (new static())->acmeHandle($path, $save);
+        $path = rtrim($path, '/');
+        $config['list'] = static::getDirFile($path);
+        $config['save'] = rtrim($save, '/');
+        foreach ($config['list'] as $k => $v) {
+            $key = strtolower($k);
+            $format = pathinfo($key, PATHINFO_EXTENSION);
+            if ($key == 'fullchain.cer') {
+                $config['full'] = $v;//完整的证书链，可更改后缀为pem。文件里一般有两段证书(也会有三张)，一张是你的域名证书，另一张是所依赖的证书链(可能会有两张证书链)
+            } else if ($key == 'ca.cer') {
+                $config['chain'] = $v;//依赖的证书链，里面内容同时存在于fullchain.crt文件中。
+            } else if ($format == 'cer') {
+                $config['domain'] = $v;//域名证书，里面内容同时存在于fullchain.crt文件中。
+            } else if ($format == 'key') {
+                $config['private'] = $v;//证书私钥，可更改后缀为key。如果使用的是自己上传的CSR文件，将不包含该文件。
+            } else if ($format == 'conf') {
+                $config['conf'] = $v;//配置
+            }
+        }
+        $pass = substr(str_shuffle('abcdefghijklmnopqrstuvwxyz1234567890'), 0, 6);
+        $save = $config['save'];
+        $all = $save . '/All';
+        $apache = $save . '/Apache';
+        $bt = $save . '/Bt';
+        $iis = $save . '/IIS';
+        $nginx = $save . '/Nginx';
+        $tomcat = $save . '/Tomcat';
+        //all
+        $pfxFile = $all . '/fullchain.pfx';
+        $passFile = $all . '/password.txt';
+        $data['all_fullchain'] = static::copyFile($config['full'], $all . '/fullchain.pem');//完整的证书链
+        $data['all_domain'] = static::copyFile($config['domain'], $all . '/domain.crt');//域名证书
+        $data['all_chain'] = static::copyFile($config['chain'], $all . '/chain.crt');//依赖的证书链
+        $data['all_private'] = static::copyFile($config['private'], $all . '/private.key');//证书私钥
+        $data['all_public'] = static::getPublic($config['private'], $all . '/public.pem');//证书公钥
+        $data['all_fullchain'] = static::getPfx($config['full'], $config['private'], $pfxFile, $pass);//PFX类型证书
+        $data['all_password'] = static::saveFile($passFile, $pass);//PFX密码
+        $time = static::getTime($config['full']);
+        $arr = array_slice(explode("DNS:", (@file_get_contents(($config['conf'] ?? '')))), 1);
+        $domain = [];
+        foreach ($arr as $v) {
+            $domain[] = trim(trim($v), ',');
+        }
+        $data['help'] = static::saveFile($all . '/help.txt',
+            static::helpTxt([
+                'chain.crt' => '依赖的证书链，里面内容同时存在于fullchain.pem文件中',
+                'domain.crt' => '域名证书，里面内容同时存在于fullchain.pem文件中',
+                'fullchain.pem' => '完整的证书链，文件里一般有两段证书(也会有三张)，一张是你的域名证书，另一张是所依赖的证书链(可能会有两张证书链)',
+                'fullchain.pfx' => 'PFX类型证书，一般IIS和Tomcat使用',
+                'password.txt' => 'PFX证书密码',
+                'private.key' => '证书私钥',
+                'public.pem' => '证书公钥',
+                '证书密码' => $pass,
+                '证书域名' => join(",", $domain),
+                '证书时间' => $time['top'] . " - " . $time['end']
+            ], true));
+        //apache
+        $data['apache_domain'] = static::copyFile($config['domain'], $apache . '/domain.crt');//域名证书
+        $data['apache_private'] = static::copyFile($config['private'], $apache . '/private.key');//证书私钥
+        $data['apache_chain'] = static::copyFile($config['chain'], $apache . '/chain.crt');//依赖的证书链
+        //bt
+        $data['bt_fullchain'] = static::copyFile($config['full'], $bt . '/fullchain.pem');//完整的证书链
+        $data['bt_private'] = static::copyFile($config['private'], $bt . '/private.key');//证书私钥
+        //iis
+        $data['tomcat_public'] = static::copyFile($pfxFile, $iis . '/fullchain.pfx');//PFX类型证书iis
+        $data['tomcat_password'] = static::copyFile($passFile, $iis . '/password.txt');//PFX密码
+        //nginx
+        $data['nginx_fullchain'] = static::copyFile($config['full'], $nginx . '/fullchain.pem');//完整的证书链
+        $data['nginx_private'] = static::copyFile($config['private'], $nginx . '/private.key');//证书私钥
+        //tomcat
+        $data['tomcat_public'] = static::copyFile($pfxFile, $tomcat . '/fullchain.pfx');//PFX类型证书iis
+        $data['tomcat_password'] = static::copyFile($passFile, $tomcat . '/password.txt');//PFX密码
+        $error = [];
+        foreach ($data as $k => $v) {
+            if ($v === 0 || empty($v)) {
+                $error[$k] = $v;
+            }
+        }
+        return ($error ?: 'success');
     }
 
     /**
-     * 通过完整的证书链 获取 证书开始到结束时间
-     * @param string $full 完整的证书链
-     * @param bool $type 是否返回全部信息
+     * acme使用dns申请命令
+     * @param string $domain 域名列表
+     * @param string $all 泛域名
+     * @param string $option 选项
+     * @return string
+     */
+    public static function acmeDnsApply(string $domain, string $all = '', string $option = "--dns dns_gd"): string {
+        return "acme.sh --issue {$domain} --force --debug 2" . (!empty($option) ? (" " . $option) : "");
+    }
+
+    /**
+     * acme使用dns更新命令
+     * @param string $domain 域名列表
+     * @param string $option 选项
+     * @return string
+     */
+    public static function acmeDnsUpdate(string $domain, string $option = "--dns dns_gd"): string {
+        return "acme.sh --renew {$domain} --force --debug 2" . (!empty($option) ? (" " . $option) : "");
+    }
+
+    /**
+     * acme申请域名命令
+     * @param string $domain //域名列表
+     * @param string $path //网站目录 绝对路径
+     * @param string $sslFile //证书保存路径
+     * @return string
+     */
+    public static function acmeApply(string $domain, string $path, string $sslFile): string {
+        return "acme.sh --issue {$domain} -w {$path} --config-home {$sslFile} --force --debug 2";
+    }
+
+    /**
+     * acme更新域名命令
+     * @param string $domain //域名列表
+     * @return string
+     */
+    public static function acmeUpdate(string $domain): string {
+        return "acme.sh --renew {$domain} --force --debug 2";
+    }
+
+    /**
+     * acme更新全部域名命令
+     * @return string
+     */
+    public static function acmeUpdateAll(): string {
+        return "acme.sh --renew-all --force --debug 2";
+    }
+
+    /**
+     * 执行命令
+     * @param string $command
      * @return array
      */
-    public static function getTime(string $full, bool $type = false): array {
-        $resource = openssl_x509_read((@file_get_contents($full)));
-        $res = openssl_x509_parse($resource);
-        $data['top_time'] = $res['validFrom_time_t'] ?? 0;
-        $data['end_time'] = $res['validTo_time_t'] ?? 0;
-        $data['top'] = date('Y-m-d H:i:s', $data['top_time']);
-        $data['end'] = date('Y-m-d H:i:s', $data['end_time']);
-        if (!empty($type)) {
-            $data['data'] = $res;
-        }
+    public static function exec(string $command): array {
+        $data['exec'] = exec($command, $output, $res);
+        $data['output'] = $output;
+        $data['res'] = $res;
+        $data['status'] = $res == 0;
         return $data;
     }
 
@@ -56,6 +174,26 @@ class SslHelper {
     public static function getPublic(string $private, string $public): bool|int {
         $res = openssl_pkey_get_details(openssl_pkey_get_private((@file_get_contents($private))));
         return static::saveFile($public, ($res['key'] ?? ''));
+    }
+
+
+    /**
+     * 通过完整的证书链 获取 证书开始到结束时间
+     * @param string $full 完整的证书链
+     * @param bool $type 是否返回全部信息
+     * @return array
+     */
+    public static function getTime(string $full, bool $type = false): array {
+        $resource = openssl_x509_read((@file_get_contents($full)));
+        $res = openssl_x509_parse($resource);
+        $data['top_time'] = $res['validFrom_time_t'] ?? 0;
+        $data['end_time'] = $res['validTo_time_t'] ?? 0;
+        $data['top'] = date('Y-m-d H:i:s', $data['top_time']);
+        $data['end'] = date('Y-m-d H:i:s', $data['end_time']);
+        if (!empty($type)) {
+            $data['data'] = $res;
+        }
+        return $data;
     }
 
     /**
@@ -141,90 +279,5 @@ class SslHelper {
             $content = $way($data, $info);
         }
         return $content;
-    }
-
-    /**
-     * @param string $path 证书位置
-     * @param string $save 保存路径
-     * @return string|array,success=成功,array=失败信息
-     */
-    private function acmeHandle(string $path, string $save): string|array {
-        $path = rtrim($path, '/');
-        $this->data['list'] = static::getDirFile($path);
-        $this->data['save'] = rtrim($save, '/');
-        foreach ($this->data['list'] as $k => $v) {
-            $key = strtolower($k);
-            $format = pathinfo($key, PATHINFO_EXTENSION);
-            if ($key == 'fullchain.cer') {
-                $this->data['full'] = $v;//完整的证书链，可更改后缀为pem。文件里一般有两段证书(也会有三张)，一张是你的域名证书，另一张是所依赖的证书链(可能会有两张证书链)
-            } else if ($key == 'ca.cer') {
-                $this->data['chain'] = $v;//依赖的证书链，里面内容同时存在于fullchain.crt文件中。
-            } else if ($format == 'cer') {
-                $this->data['domain'] = $v;//域名证书，里面内容同时存在于fullchain.crt文件中。
-            } else if ($format == 'key') {
-                $this->data['private'] = $v;//证书私钥，可更改后缀为key。如果使用的是自己上传的CSR文件，将不包含该文件。
-            } else if ($format == 'conf') {
-                $this->data['conf'] = $v;//配置
-            }
-        }
-        $pass = substr(str_shuffle('abcdefghijklmnopqrstuvwxyz1234567890'), 0, 6);
-        $save = $this->data['save'];
-        $apache = $save . '/Apache';
-        $bt = $save . '/Bt';
-        $nginx = $save . '/Nginx';
-        $iis = $save . '/IIS';
-        $all = $save . '/All';
-        $tomcat = $save . '/Tomcat';
-        //apache
-        $data['apache_domain'] = static::copyFile($this->data['domain'], $apache . '/domain.crt');//域名证书
-        $data['apache_private'] = static::copyFile($this->data['private'], $apache . '/private.key');//证书私钥
-        $data['apache_chain'] = static::copyFile($this->data['chain'], $apache . '/chain.crt');//依赖的证书链
-        //bt
-        $data['bt_fullchain'] = static::copyFile($this->data['full'], $bt . '/fullchain.pem');//完整的证书链
-        $data['bt_private'] = static::copyFile($this->data['private'], $bt . '/private.key');//证书私钥
-        //iis
-        $pfx = $iis . '/fullchain.pfx';
-        $data['iis_fullchain'] = static::getPfx($this->data['full'], $this->data['private'], $pfx, $pass);//PFX类型证书
-        $data['iis_password'] = static::saveFile($iis . '/password.txt', $pass);//PFX密码
-        //nginx
-        $data['nginx_fullchain'] = static::copyFile($this->data['full'], $nginx . '/fullchain.pem');//完整的证书链
-        $data['nginx_private'] = static::copyFile($this->data['private'], $nginx . '/private.key');//证书私钥
-        //tomcat
-        $data['tomcat_public'] = static::copyFile($pfx, $tomcat . '/fullchain.pfx');//PFX类型证书
-        $data['tomcat_password'] = static::saveFile($tomcat . '/password.txt', $pass);//PFX密码
-        //all
-        $data['all_fullchain'] = static::copyFile($this->data['full'], $all . '/fullchain.pem');//完整的证书链
-        $data['all_domain'] = static::copyFile($this->data['domain'], $all . '/domain.crt');//域名证书
-        $data['all_chain'] = static::copyFile($this->data['chain'], $all . '/chain.crt');//依赖的证书链
-        $data['all_private'] = static::copyFile($this->data['private'], $all . '/private.key');//证书私钥
-        $data['all_public'] = static::getPublic($this->data['private'], $all . '/public.pem');//证书公钥
-        $data['all_public'] = static::copyFile($pfx, $all . '/fullchain.pfx');//PFX类型证书
-        $data['all_password'] = static::saveFile($all . '/password.txt', $pass);//PFX密码
-        $time = static::getTime($this->data['full']);
-        $arr = array_slice(explode("DNS:", (@file_get_contents(($this->data['conf'] ?? '')))), 1);
-        $domain = [];
-        foreach ($arr as $v) {
-            $domain[] = trim(trim($v), ',');
-        }
-        $data['help'] = static::saveFile($all . '/help.txt',
-            static::helpTxt([
-                'chain.crt' => '依赖的证书链，里面内容同时存在于fullchain.pem文件中',
-                'domain.crt' => '域名证书，里面内容同时存在于fullchain.pem文件中',
-                'fullchain.pem' => '完整的证书链，文件里一般有两段证书(也会有三张)，一张是你的域名证书，另一张是所依赖的证书链(可能会有两张证书链)',
-                'fullchain.pfx' => 'PFX类型证书，一般IIS和Tomcat使用',
-                'password.txt' => 'PFX证书密码',
-                'private.key' => '证书私钥',
-                'public.pem' => '证书公钥',
-                '证书密码' => $pass,
-                '证书域名' => join(",", $domain),
-                '证书时间' => $time['top'] . " - " . $time['end']
-            ], true));
-        $error = [];
-        foreach ($data as $k => $v) {
-            if ($v === 0 || empty($v)) {
-                $error[] = $k;
-            }
-        }
-        return (!empty($error) ? $error : 'success');
     }
 }
